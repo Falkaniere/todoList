2987a8c2bb36817ee8851f726ba5d58b63053c6f
jonatas falkaniere <jonatasfalkaniere@gmail.com>
2024-01-29 20:31:04 -0300

    Initial Commit

---
+++ toDoList/toDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  toDoListApp.swift
+//  toDoList
+//
+//  Created by Jonatas Falkaniere on 29/01/24.
+//
+
+import SwiftUI
+
+@main
+struct toDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
